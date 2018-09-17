29210016ddbc6f616377b5e37dd5679aa47a9247
Nilseu Padilha <nilseu.junior@aurea.com>
2018-09-17 15:37:36 -0300

    first commit

---
+++ src/main/java/Class1.java
@@ -0,0 +1,24 @@
+public final class Class1 {
+
+    public static void print() {
+        System.out.println("print 1");
+        System.out.println("print 2");
+        System.out.println("print 3");
+        System.out.println("print 4");
+        System.out.println("print 5");
+    }
+
+    public static void print2() {
+        System.out.println("print a");
+        System.out.println("print b");
+        System.out.println("print c");
+        System.out.println("print d");
+        System.out.println("print e");
+
+
+        System.out.println("print 1 1");
+        System.out.println("print 1 2");
+        System.out.println("print 1 3");
+    }
+
+}
---
+++ src/main/java/DuplicateCode.java
@@ -0,0 +1,14 @@
+public class DuplicateCode {
+
+    public static void main(String[] args) {
+
+        Class1.print();
+        Class1.print2();
+
+        Class2.print();
+        Class2.print2();
+
+        Class3.print();
+        Class3.print2();
+    }
+}
